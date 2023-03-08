8a8854ea091cf67c48c4f785a1afb5341a3d299f
RafaelYukio <rafaelyut@hotmail.com>
2023-03-08 15:06:57 -0300

    feat: implementado geração da matriz para o campo minado

--- src/components/MineSquare/index.tsx
+++ src/components/MineSquare/index.tsx
@@ -4,30 +4,87 @@ import * as S from "./styles";
 const MineSquare = () => {
   const inputRefRow = useRef<HTMLInputElement>(null);
   const inputRefColumn = useRef<HTMLInputElement>(null);
+  const inputRefQuantity = useRef<HTMLInputElement>(null);
 
-  const [generateColumns, setGenerateColumns] = useState([1, 2, 3, 4]);
-  const [generateMines, setGenerateMines] = useState([1, 2, 3, 4]);
+  const [matrix, setMatrix] = useState<Array<Array<number>>>(
+    Array(6).fill(Array(6).fill(0))
+  );
 
-  const getRandomInt = (): number => Math.floor(Math.random() * 2);
+  const getRandomInt = (max: number): number => Math.floor(Math.random() * max);
 
   const handleChange = () => {
-    let updatedColumns: Array<number> = [];
-    let updatedMines: Array<number> = [];
+    // Recebe valores dos inputs
+    let inputColumn: number = parseInt(inputRefColumn.current?.value || "0");
+    let inputRow: number = parseInt(inputRefRow.current?.value || "0");
+    let inputQuantityPercentage: number = parseInt(
+      inputRefQuantity.current?.value || "0"
+    );
 
-    if (inputRefRow.current && inputRefColumn.current) {
-      for (let i = 0; i < parseInt(inputRefColumn.current.value); i++) {
-        updatedColumns.push(getRandomInt());
-      }
-      for (let i = 0; i < parseInt(inputRefRow.current.value); i++) {
-        updatedMines.push(getRandomInt());
-      }
+    // Monta a matriz temporária de acordo com tamanho setados nos inputs
+    // Obs.: o método 'Array(len).fill(Array(len).fill(0))' não funciona, pois é gerado a mesma Array de referência:
+    // https://stackoverflow.com/questions/57514341/setting-the-value-in-a-matrix-after-fill-returns-incorrect-matrix
+    let tempMatrix: Array<Array<number>> = Array.from(
+      { length: inputColumn },
+      (x) => new Array(inputRow).fill(0)
+    );
+
+    // Exemplo de matriz:
+
+    //    A array dentro da  array principal define a quantidade de linhas
+    //    |
+    // [ [0 , [0 , [0 , [0 ]   <- Array principal define a quantidade de colunas
+    //    0    0    0    0
+    //    0    0    0    0
+    //    0]   0]   0]   0]
+
+    // Calcula quantidade de mines de acordo com tamanho da matriz
+    let mineQuantity: number = Math.floor(
+      inputColumn * inputRow * (inputQuantityPercentage / 100)
+    );
+
+    // Coloca mines em lugares aleatórios da matriz
+    // O local da mine terá o número 7
+    for (let i: number = 0; i < mineQuantity; i++) {
+      let columnCoordinate: number = getRandomInt(inputColumn);
+      let rowCoordinate: number = getRandomInt(inputRow);
+
+      tempMatrix[columnCoordinate][rowCoordinate] = 7;
     }
 
-    setGenerateColumns(updatedColumns);
-    setGenerateMines(updatedMines);
+    // Faz uma verificação por coluna para colocar as dicas de onde estão as mines
+    tempMatrix.forEach((currentArrayRow, columnIndex) => {
+      // Encontra os indexes das mines desta coluna
+      let mineIndexes: Array<number> = currentArrayRow.reduce(
+        (accumulator, currentValue, currentIndex) => {
+          if (currentValue === 7) accumulator.push(currentIndex);
+          return accumulator;
+        },
+        new Array<number>()
+      );
 
-    console.log(updatedColumns, updatedMines);
-    console.log(generateColumns, generateMines);
+      // Adiciona +1 em volta de cada mine desta coluna
+      // [ [+1 , [+1 , [+1 ]
+      //    +1     7    +1
+      //    +1]   +1]   +1]
+
+      for (let i = -1; i < 2; i++) {
+        let arrayRow: Array<number> = tempMatrix[columnIndex + i];
+
+        if (arrayRow) {
+          mineIndexes.forEach((mineIndex) => {
+            for (let i = -1; i < 2; i++) {
+              if (
+                arrayRow[mineIndex + i] !== undefined &&
+                arrayRow[mineIndex + i] !== 7
+              )
+                arrayRow[mineIndex + i]++;
+            }
+          });
+        }
+      }
+    });
+
+    setMatrix(tempMatrix);
   };
 
   return (
@@ -36,11 +93,13 @@ const MineSquare = () => {
       <input ref={inputRefColumn} type="number" />
       Linhas
       <input ref={inputRefRow} type="number" />
+      Bombas (%)
+      <input ref={inputRefQuantity} type="number" />
       <button onClick={handleChange}>-</button>
       <S.SquareDiv>
-        {generateColumns.map(() => (
+        {matrix.map((content, index) => (
           <S.ColumnDiv key={Math.random()}>
-            {generateMines.map((number) => (
+            {matrix[index].map((number) => (
               <S.MineButton key={Math.random()}>{number}</S.MineButton>
             ))}
           </S.ColumnDiv>
